3c01c52b816038b435b463518fcfc78dd170482e
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-01-18 12:28:30 +0200

    Setup tests and add a simple route redirect test

--- ckanext/collection/tests/test_plugin.py
+++ ckanext/collection/tests/test_plugin.py
@@ -1,5 +1,17 @@
-"""Tests for plugin.py."""
-import ckanext.collection.plugin as plugin
+from nose import tools as nosetools
 
-def test_plugin():
-    pass
+import logging
+log = logging.getLogger(__name__)
+
+import ckan.tests.helpers as helpers
+
+submit_and_follow = helpers.submit_and_follow
+
+
+class TestCollectionIndex(helpers.FunctionalTestBase):
+
+    def test_collections_redirects_to_collection(self):
+        '''/collections redirects to /collection.'''
+        app = self._get_test_app()
+        response = app.get('/collections', status=302)
+        nosetools.assert_equal(response.location, 'http://localhost/collection')
