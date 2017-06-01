c842512e02fd0d89812d82261bf69fd1f70247e9
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-06-01 14:52:22 +0300

    Call package_search separately to add package_count to api_collection_show

--- ckanext/collection/logic/action.py
+++ ckanext/collection/logic/action.py
@@ -8,12 +8,24 @@ get_action = logic.get_action
 def api_collection_show(context, data_dict):
     data_dict = {
         'include_extras': False,
-        'include_datasets': False,
-        'include_dataset_count': True,
+        'include_datasets': False, # Set to False to prevent group_dictize from calling package_search
+        'include_dataset_count': False, # Set to False to prevent group_dictize from calling package_search
         'include_users': False,
         'include_groups': False,
         'include_followers': False,
         'include_tags': False,
         'id': config.get('ckanext.collection.api_collection_name_or_id')
     }
-    return get_action('group_show')(context, data_dict)
+    collection = get_action('group_show')(context, data_dict)
+
+    # group_dictize (called in group_show) does not support group types so a separate package_search is needed
+    # to include the package count
+    if collection:
+        search_dict = {
+            'fq': 'collections:' + collection['name'],
+            'include_private': False,
+        }
+        collection_packages = get_action('package_search')(context, search_dict)
+        collection['package_count'] = collection_packages['count']
+
+    return collection
